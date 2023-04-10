7e00249e299d969b2e4ab6a9b48f8ce7b0228f0f
Rattanachai <zrattanachai.m@pttdigital.com>
2023-04-10 11:45:56 +0700

    Lavender_Update
    Lavender_Update

--- Lavender_Update/app.js
+++ Lavender_Update/app.js
@@ -6,41 +6,32 @@ const io_client = require('socket.io-client');
 const service = require('./services/ScheduleJob')
 const { ScheduleJob } = require('./controller/Functions')
 
-
-//let client_id = "I101";
-//const socket_client = io_client('http://10.230.25.113:3500',{
-//  extraHeaders: {
-//    "clientid": get_site_name
-//  }
-//});
-// app.use(cors());
-//socket_client.on('request', (msg) => {
-//  ScheduleJob(socket_client, msg);
-//});
-
-//console.log(`---${config.name} Service ---`)
-//console.log(`Connecting to ${config.name} repository...`)
-//console.log(`Open Service By Port ${config.serverSettings.port} Success`);
-//console.log(`Time Now :` + moment().format('YYYY-MM-DD HH:mm:ss'));
-//service.SetScheduleJob();
-
 async function Run() {
   const clientid = await config.getSite
   const socket_client = await io_client('http://10.230.25.113:3500', {
     extraHeaders: {
-      "clientid": clientid
+      "clientid": clientid,
+      "token": "sdddf"
     }
   });
-
-  socket_client.on('request', (msg) => {
-    ScheduleJob(socket_client, msg);
-  });
+  
+     socket_client.on('request', (msg) => {
+      ScheduleJob(socket_client, msg);
+    });
 
   console.log(`---${config.name} Service ---`)
   console.log(`Connecting to ${config.name} repository...`)
   console.log(`Open Service By Port ${config.serverSettings.port} Success`);
   console.log(`Time Now :` + moment().format('YYYY-MM-DD HH:mm:ss'));
   service.SetScheduleJob();
+}
+
+async function getToken() {
+  token_expires = {
+      "siteCode" : await config.getSite,
+      "messageType": "token expires"
+    }
+  socket_client.emit("request",token_expires);
 }
 
 Run()
